686f3f1ca93508c88dd632657cd0d5d3276fdfe2
korvlad21 <vladek2000@inbox.ru>
2022-07-25 22:21:59 +0300

    15 урок начало

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+console.log('is running');
+Vue.component('home', require('./components/Home.vue').default);
+
+const app = new Vue({
+    el: "#app"
+});
+
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>
+        CRM Helpdesc
+    </h1>
+</template>
+
+<script>
+    export default{
+        name: "Home"
+    }
+</script>
+<style scoped>
+
+</style>
